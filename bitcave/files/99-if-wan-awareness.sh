273da0949892bf42741c586ff1bb6df71fcf1389
Matthias Strubel <matthias.strubel@aod-rpg.de>
2015-01-07 17:24:50 +0100

    Included hotplug file for openvpn reload on WAN line disconnect

---
+++ bitcave/files/99-if-wan-awareness.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#!/bin/sh 
+#[ "$ACTION" = ifup ] && { logger -t button-hotplug Device: $DEVICE / Action: $ACTION 
+#}
+
+logger -t DEBUG "Starting hotplug for changing "
+
+if [ "$INTERFACE" = "wan" ] ; then
+	logger -t DEBUG "Found wan event"
+	if [ "$ACTION" = "ifup" ] ; then
+		logger -t "IFUP event fired.."
+		/etc/init.d/openvpn start
+	fi
+	if [ "$ACTION" = "ifdown" ] ; then
+		logger -t "IFDOWN event fired.."
+		/etc/init.d/openvpn stop :q
+
+	fi
+fi
